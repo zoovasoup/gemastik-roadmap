b0e49e00cbec37df4167dc41c69f701360a63f2d
zoovasoup <zoovasoup@proton.me>
2026-04-14 01:24:53 +0000

    [add] allow network for dev preview

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   typedRoutes: true,
   reactCompiler: true,
   transpilePackages: ["shiki"],
+  allowedDevOrigins: ['100.70.142.22']
 };
 
 export default nextConfig;
